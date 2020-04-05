a7058db04903b6c8029a73f6481b8c190fab1306
CrioDo <rahul@criodo.com>
2020-04-05 15:09:11 +0530

    Updates

--- precache-manifest.d67e0c3c5723fddf007206505b6fa556.js
+++ precache-manifest.d67e0c3c5723fddf007206505b6fa556.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f48d0a1bcb326dd2b588ea57d0255c0",
+    "revision": "d04b08044c147c2b3d07fcc50b1d1665",
     "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/index.html"
   },
   {
-    "revision": "ef7d648112df48b0e375",
+    "revision": "5c0192b780a02a92dbde",
     "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/css/2.57ddf7ad.chunk.css"
   },
   {
-    "revision": "5d0f7a186b02f720a6ec",
+    "revision": "7c79ee40c02bc814569a",
     "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/css/main.5db1380d.chunk.css"
   },
   {
-    "revision": "ef7d648112df48b0e375",
-    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/2.6de623bb.chunk.js"
+    "revision": "5c0192b780a02a92dbde",
+    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/2.6e981311.chunk.js"
   },
   {
     "revision": "d466ab9648f013dea081956fc7c904f3",
-    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/2.6de623bb.chunk.js.LICENSE.txt"
+    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/2.6e981311.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5d0f7a186b02f720a6ec",
-    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/main.8fa6b4d3.chunk.js"
+    "revision": "7c79ee40c02bc814569a",
+    "url": "/Crio-Launch-Feb-2020-dpk10-iitbbs-ac/static/js/main.94a26e16.chunk.js"
   },
   {
     "revision": "b4094ef8a0c98c3324ca",
